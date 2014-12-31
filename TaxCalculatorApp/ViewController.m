d867b6291b9c0e3bf3b8278b05cda24fde092178
Syed Amaanullah <syedamaanullah@syeds-mbp.attlocal.net>
2014-12-31 15:14:42 -0500

    first commit

--- TaxCalculatorApp/ViewController.m
+++ TaxCalculatorApp/ViewController.m
@@ -9,19 +9,93 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
+
+- (IBAction)indexChanged:(UISegmentedControl *)sender;
+
+
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    CGRect frame= self.segmentedControl.frame;
+    [self.segmentedControl setFrame:CGRectMake(frame.origin.x, frame.origin.y, frame.size.width, 250)];
+
+    self.caTax = .075;
+    self.chiTax = .0925;
+    self.nyTax = .045;
+
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)indexChanged:(UISegmentedControl *)sender {
+    NSString *input = self.priceTextField.text;
+    double inputValue = input.intValue;
+
+    switch (self.segmentedControl.selectedSegmentIndex) {
+        case 0:
+        {
+            double result = inputValue * self.caTax;
+            self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+            break;
+        }
+        case 1:
+        {
+            double result = inputValue * self.chiTax;
+            self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+        }
+        case 2:
+        {
+            double result = inputValue * self.nyTax;
+            self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+        }
+        default:
+            break;
+    }
+
 }
+
+
+
+- (IBAction)onCalculateButtonTapped:(id)sender
+{
+    NSString *input = self.priceTextField.text;
+    double inputValue = input.intValue;
+
+    switch (self.segmentedControl.selectedSegmentIndex) {
+        case 0:
+        {
+            double result = inputValue * self.caTax;
+            self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+            break;
+        }
+        case 1:
+        {
+            double result = inputValue * self.chiTax;
+            self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+        }
+        case 2:
+        {
+            double result = inputValue * self.nyTax;
+            self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+        }
+        default:
+            break;
+    }
+
+    }
+
+
 
 @end
